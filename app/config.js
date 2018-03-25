885c440714cbc5051ec01b8d3e8edd137e733d6c
Juan Saudade <saudadista@gmail.com>
2018-03-25 02:03:11 -0500

    mod

--- app/config.js
+++ app/config.js
@@ -6,5 +6,5 @@
 // 	mapboxApiKey: ""
 // };
 window.ushahidi = {
-    platform_websocket_redis_adapter_url: 'http://127.0.0.1:3009'
+    platform_websocket_redis_adapter_url: 'http://127.0.0.1:3000'
 };
