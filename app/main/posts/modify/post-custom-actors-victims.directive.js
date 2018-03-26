6aa48efe8dab58f12d63c800c73bdf0db767f0ee
Juan Saudade <developer@kuery.com>
2018-03-26 02:41:10 -0500

    mod

--- app/main/posts/modify/post-custom-actors-victims.directive.js
+++ app/main/posts/modify/post-custom-actors-victims.directive.js
@@ -26,13 +26,39 @@ function PostCustomActorsVictimsController(
     $sce,
     Notify
 ) {
+    $scope.switchTab = switchTab;
+    activate();
 
     var initCategory = function (){
         if ($scope.selected_categories && $scope.selected_categories.length > 0) {
             $scope.categoria = $scope.selected_categories[0].id;
         }
+    }
+
+    function activate() {
+            $scope.tab_history = {};
+
+        // Set initial menu tab
+            $scope.switchTab('post', 'actor');
+        }
+
+    function switchTab(section, tab) {
+
+        // First unset last active tab
+        var old_tab = $scope.tab_history[section];
+        if (old_tab) {
+            var old_tab_li = old_tab + '-li';
+            angular.element(document.getElementById(old_tab)).removeClass('active');
+            angular.element(document.getElementById(old_tab_li)).removeClass('active');
+        }
+        // Set new active tab
+        tab = tab + '-' + section;
+        $scope.tab_history[section] = tab;
+        var tab_li = tab + '-li';
+        angular.element(document.getElementById(tab)).addClass('active');
+        angular.element(document.getElementById(tab_li)).addClass('active');
     } 
-    
+
     $rootScope.$on('selected_category', function(e){
         var selected = e.targetScope.selected;
         var categories = e.targetScope.categories;
